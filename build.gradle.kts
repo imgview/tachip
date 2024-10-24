6dfb869a4654cd9f8e6fa44918c83f207981f3bd
imgview <komiklah007@gmail.com>
2024-10-24 12:40:02 +0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,29 +1,7 @@
-buildscript {
-    repositories {
-        mavenCentral()
-        google()
-        maven(url = "https://plugins.gradle.org/m2/")
-    }
-    dependencies {
-        classpath(libs.gradle.agp)
-        classpath(libs.gradle.kotlin)
-        classpath(libs.gradle.serialization)
-        classpath(libs.gradle.kotlinter)
-    }
-}
-
 allprojects {
     repositories {
         mavenCentral()
         google()
-        maven(url = "https://jitpack.io") // Menambahkan repositori JitPack di sini
+        maven(url = "https://jitpack.io")
     }
-}
-
-dependencies {
-    implementation("com.github.imgview:tachip:1.0.0") // Ganti '1.0.0' dengan versi yang sesuai
-}
-
-tasks.register<Delete>("clean") {
-    delete(rootProject.layout.buildDirectory.asFile.get())
 }
